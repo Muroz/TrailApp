933fabfa58bd2d621f1fc4173da521553ee59749
Diego Zuluaga <diegozuluaga@Diegos-MacBook-Air.local>
2017-04-25 14:47:26 -0230

    Second commit, added image views to the tables and working pop up

--- eastCoastTrailApp/FirstViewController.swift
+++ eastCoastTrailApp/FirstViewController.swift
@@ -8,17 +8,59 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    var popViewController: PopUpViewController!
+    
+    var placemarks: [String] = ["spout.jpg", "lamanche.jpg", "capeSpear.jpg"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //Storyboard
+        let viewControllerStoryboardId = "PopUpViewController"
+        let storyboardName = "Main"
+        let storyboard = UIStoryboard(name: storyboardName, bundle: Bundle.main)
+        popViewController = (storyboard.instantiateViewController(withIdentifier: viewControllerStoryboardId) as UIViewController!) as! PopUpViewController!
+        //popViewController = PopUpViewController(nibName: "PopUpViewController", bundle: bundle)
+        popViewController.title = "This is a popup view"
+        popViewController.showInView(aView: self.view, withImage: UIImage(named: "eastcoastt1.jpg"), withMessage: "You just triggered a great popup window", animated: true)
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return placemarks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "MyTestCell")
+        
+        cell.textLabel?.text = "Row \(indexPath.row)"
+        cell.detailTextLabel?.text = "Subtitle \(indexPath.row)"
+        
+        var image : UIImage = UIImage(named: placemarks[indexPath.row])!
+
+        //var frame = CGRect(x: 0, y: 0, width: 200, height: 50)
+        
+        
+        
+        cell.imageView?.image = image
+        cell.imageView?.contentMode = .scaleAspectFill
+        
+        return cell
+    }
+    
+    
+    public func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat{
+        return 100
+    }
+
 
 
 }
---
+++ eastCoastTrailApp/PopUpViewController.swift
@@ -0,0 +1,78 @@
+//
+//  PopUpViewController.swift
+//  eastCoastTrailApp
+//
+//  Created by Diego Zuluaga on 2017-04-25.
+//  Copyright © 2017 Diego Zuluaga. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PopUpViewController : UIViewController {
+    
+    @IBOutlet weak var logoImage: UIImageView!
+
+    @IBOutlet weak var messageLabel: UITextView!
+
+    @IBOutlet weak var popUpView: UIView!
+    
+    required public init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+    }
+    
+    override init(nibName nibNameOrNil: String!, bundle nibBundleOrNil: Bundle!) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+        //var backG = UIColor(patternImage: UIImage(named: "camBack.jpg")!)
+        self.popUpView.backgroundColor = UIColor(white:1,alpha:0.5)
+        self.popUpView.layer.cornerRadius = 5
+        self.popUpView.layer.shadowOpacity = 0.8
+        self.popUpView.layer.shadowOffset = CGSize(width:0.0, height:0.0)
+    }
+    
+    open func showInView(aView: UIView!, withImage image : UIImage!, withMessage message: String!, animated: Bool)
+    {
+        aView.addSubview(self.view)
+        logoImage!.image = image
+        messageLabel!.text = message
+        if animated
+        {
+            self.showAnimate()
+        }
+    }
+
+    func showAnimate()
+    {
+        self.view.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+        self.view.alpha = 0.0;
+        UIView.animate(withDuration: 0.25, animations: {
+            self.view.alpha = 1.0
+            self.view.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+        });
+    }
+    
+    func removeAnimate()
+    {
+        UIView.animate(withDuration: 0.25, animations: {
+            self.view.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+            self.view.alpha = 0.0;
+        }, completion:{(finished : Bool)  in
+            if (finished)
+            {
+                self.view.removeFromSuperview()
+            }
+        });
+    }
+    
+    @IBAction func closePopup(sender: AnyObject){
+        self.removeAnimate()
+    }
+    
+
+
+}
