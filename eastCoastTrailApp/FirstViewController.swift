e8258ac4950bf1d5c6ef1197ac9e8842239bee88
Diego Zuluaga <diegozuluaga@Diegos-MacBook-Air.local>
2017-04-25 13:59:55 -0230

    Initial Commit

---
+++ eastCoastTrailApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  eastCoastTrailApp
+//
+//  Created by Diego Zuluaga on 2017-04-25.
+//  Copyright © 2017 Diego Zuluaga. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ eastCoastTrailApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  eastCoastTrailApp
+//
+//  Created by Diego Zuluaga on 2017-04-25.
+//  Copyright © 2017 Diego Zuluaga. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
